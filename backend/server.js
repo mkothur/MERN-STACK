62f4e71f57eff0fbc62ed1b13c6453c9c19d6615
mkothur <mkmudumal25@gmail.com>
2023-09-09 18:30:26 -0400

    MERN stack REST APIs for Workout app.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // Express app
 const app = express();
@@ -19,11 +20,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/workouts', workoutRoutes)
-// app.get('/', (req, res) => {
-//     res.json({
-//         msg: 'Welcome to the app'
-//     })
-// })
+app.use('/api/user', userRoutes)
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI)
